2a88d4842585d152c95edd6f0e45137c051b8722
ryu <liuyouchao111@gmail.com>
2015-11-08 21:24:11 +0800

    improve productivity

--- helm-init.el
+++ helm-init.el
@@ -6,9 +6,13 @@
 (require 'ggtags)
 (require 'helm-gtags)
 (require  'helm-config)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
 (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
 (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
 
 (define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
 
+(helm-mode 1)                                         ;; global enable helm mode
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
