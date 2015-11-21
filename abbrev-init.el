827205d5847ae00d1bc9a25e0cde8f1ae0effae9
ryu <liuyouchao111@gmail.com>
2015-11-21 23:35:52 +0800

    Mac OS X key support in org mode

--- abbrev-init.el
+++ abbrev-init.el
@@ -1,7 +1,7 @@
-(setq abbrev-file-name (concat my-emacs-path "abbrev_defs"))
+(setq abbrev-file-name (concat my-emacs-path "abbrev-defs.el"))
 ;; reads the abbreviations file on startup,like M-x read-abbrev-file
 (quietly-read-abbrev-file)
 ;; turn on abbrev mode globally
-;; (setq-default abbrev-mode t)
+(setq-default abbrev-mode t)
 
 (setq save-abbrevs t)              ;; save abbrevs when files are saved
